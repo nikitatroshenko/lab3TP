83da749cad0e1a3ee4bf2d25ee60efaa7293ec1c
VenskiyIlya <vensilya@gmail.com>
2016-03-03 16:54:29 +0300

    inspection

--- task1.c
+++ task1.c
@@ -3,7 +3,15 @@
 int main()
 {
     int age;							//integer field age
-    printf("Enter your age: ");					//printf instruction
-    scanf("%d", &age);						//user enter age
+    while(True)
+    {
+        printf("Enter your age: ");				//printf instruction
+        scanf("%d", &age);					//user enter age
+        if(age > 0)
+        {
+            break;
+        }
+    }								
     printf("Your age in days: %d\n", age * 365 + age /4);	//prints age in days
 }
+
