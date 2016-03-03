71e70b460f5563993a46cdc5c8cb7e97d3070008
VenskiyIlya <vensilya@gmail.com>
2016-03-03 16:46:17 +0300

    docs for the project

--- task1.c
+++ task1.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-    int age;
-    printf("Enter your age: ");
-    scanf("%d", &age);
-    printf("Your age in days: %d\n", age * 365 + age /4);
+    int age;							//integer field age
+    printf("Enter your age: ");					//printf instruction
+    scanf("%d", &age);						//user enter age
+    printf("Your age in days: %d\n", age * 365 + age /4);	//prints age in days
 }
